fc7d6f9248d576065a368d5fecc15f2689167efe
andrehsvictor <andrehsvictor@gmail.com>
2025-01-01 10:40:19 -0300

    feat: Initialize Spring Boot project

---
+++ src/test/java/andrehsvictor/anibaze/AnibazeApplicationTests.java
@@ -0,0 +1,13 @@
+package andrehsvictor.anibaze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnibazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
