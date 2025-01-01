d4b2f945e0f579949f0b6451b23af1068914e922
andrehsvictor <andrehsvictor@gmail.com>
2025-01-01 20:22:35 -0300

    feat: Add FieldErrorDto for error handling in API responses

---
+++ src/main/java/andrehsvictor/anibaze/dto/error/FieldErrorDto.java
@@ -0,0 +1,11 @@
+package andrehsvictor.anibaze.dto.error;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FieldErrorDto {
+    private String field;
+    private String message;
+}
