eeb075e29b81c8a9a7597e56a937a2437bc8952d
andrehsvictor <andrehsvictor@gmail.com>
2025-01-01 20:22:31 -0300

    feat: Add Payload class for standardized API responses

---
+++ src/main/java/andrehsvictor/anibaze/dto/Payload.java
@@ -0,0 +1,51 @@
+package andrehsvictor.anibaze.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Payload<T, E> {
+    private T data;
+    private Long totalElements;
+    private Integer totalPages;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Boolean hasNext;
+    private Boolean hasPrevious;
+    private String sort;
+    private List<E> errors = new ArrayList<>();
+
+    public static Payload<?, String> error(String message) {
+        Payload<?, String> payload = new Payload<>();
+        payload.getErrors().add(message);
+        return payload;
+    }
+
+    public static <T> Payload<T, ?> success(T data) {
+        Payload<T, ?> payload = new Payload<>();
+        payload.setData(data);
+        return payload;
+    }
+
+    public static <T> Payload<Page<T>, ?> success(Page<T> page) {
+        Payload<Page<T>, ?> payload = new Payload<>();
+        payload.setData(page);
+        payload.setTotalElements(page.getTotalElements());
+        payload.setTotalPages(page.getTotalPages());
+        payload.setPageNumber(page.getNumber());
+        payload.setPageSize(page.getSize());
+        payload.setHasNext(page.hasNext());
+        payload.setHasPrevious(page.hasPrevious());
+        payload.setSort(page.getSort().toString());
+        return payload;
+    }
+}
